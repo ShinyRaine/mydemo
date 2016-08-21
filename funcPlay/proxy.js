72402209920e9d051788a76e3fddc8cb9c7cb0f3
rizzle <734783679@qq.com>
2016-08-21 11:32:05 +0800

    proxy

--- funcPlay/proxy.js
+++ funcPlay/proxy.js
@@ -38,5 +38,35 @@ var proxySendFile = (function(){
 		}
 })()
 
-// 惰性加载
+// 缓存代理
+
+var mult = function () {
+	var a = 1
+	for (var i = 0; i < arguments.length; i++) {
+		a = a * arguments[i]
+	}
+	return a
+}
+var proxyMult = (function() {
+	var cache = {}
+	return function() {
+		var args = Array.prototype.join.call(arguments,',')
+		if (args in cache) {
+			return cache[args]
+		}
+		return cache[args] = mult.apply(this, arguments)
+	}
+})()
+// 一个创建代理函数的函数
+var proxyFactory = function(fn) {
+	var cache = {}
+	return function() {
+		var args = Array.prototype.join.call(arguments,',')
+		if (args in cache) {
+			return cache[args]
+		}
+		return cache[args] = fn.apply(this, arguments)
+	}
+}
+
 
