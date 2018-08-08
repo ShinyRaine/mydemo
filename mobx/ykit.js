544a1f57c8863c4076d191ecaf381c5f8138800b
xinyu.cheng <xinyu.cheng@qunar.com>
2018-08-08 19:52:14 +0800

    del css

--- mobx/ykit.js
+++ mobx/ykit.js
@@ -1,18 +1,13 @@
 'use strict';
 
 module.exports = {
-    plugins: ['qunar', 'react'],
+    plugins: ['react'],
     config: {
         exports: [
-            './scripts/index.js',
-            './styles/index.scss'
+            './scripts/index.js'
         ],
         modifyWebpackConfig: function(baseConfig) {
             return baseConfig
-        },
-        sync: {
-            host : "192.168.237.71",
-            path: "/home/q/www/qunarzz.com/ykit-seed-qunar"
         }
     },
     hooks: {},
